d766859dbd32da08b52d58c3fc5993f39c727dab
rodrigopaiva <rodrigofpaiva@gmail.com>
2015-05-21 00:48:38 -0300

    base adicionada e inicio elementos separados

---
+++ elements/botoes.php
@@ -0,0 +1,34 @@
+<?php include_once "../_inc/headlines.php"; ?>
+
+
+    <body>
+        <!--[if lt IE 10]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="all">
+
+        	<section class="main">
+                <header>
+                    <h1>botoes</h1>
+                </header>
+
+                <article>
+                    <button class="btn">botao default</button>
+                    <button class="btn btn-success">botao success</button>
+                    <button class="btn btn-warning">botao warning</button>
+                    <button class="btn btn-error">botao error</button>
+                    <button class="btn btn-info">botao info</button>
+                    <button class="btn btn-round">botao round</button>
+                    <button class="btn btn-radius">botao radius</button>
+
+                    <button class="btn close">&times;</button>
+                    <a class="close" href="#">&times;</a>
+                </article>
+        	</section>
+
+		</div><!-- end all -->
+
+        <?php include_once "../_inc/scripts.php"; ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,72 +1,8 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="pt-br"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta http-equiv="content-language" content="pt-br" />
-        <meta http-equiv="cache-control" content="no-cache" />
-
-        <title>titulo do projeto</title>
-
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <!-- META TAGS -->
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-touch-fullscreen" content="yes" />
-        <meta name="HandheldFriendly" content="true" />
-        <meta name="format-detection" content="telephone=yes" />
-        <meta name="DC.title" content="titulo do projeto" />
-        <meta name="DC.publisher" content="nome do desenvolvedor" />
-        <meta name="DC.creator.address" content="dev_name@gmail.com" />
-        <meta name="robots" content="Index" />
-        <meta name="author" content="nome do desenvolvedor" />
-        <meta name="rating" content="general" />
-        <meta name="distribution" content="global" />
-        <meta name="copyright" content="titulo do projeto - Todos os direitos reservados" />
-
-        <!-- Team -->
-        <link rel="author" type="text/plain" href="humans.txt" />
-
-        <!-- Favicons -->
-        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico?v=2">
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="touch-icon-ipad.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="touch-icon@2x.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad@2x.png">
-
-        <!-- Facebook Tags -->
-        <meta property="og:description" content="Descricao.">
-        <meta property="og:image" content="http://www.meusite.com.br/images/thumbnail-share.jpg">
-        <meta property="og:site_name" content="titulo do site">
-        <meta property="og:title" content="titulo do site">
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="http://www.meusite.com.br/index.php">
-
-        <!-- Twitter tags -->
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:site" content="http://www.meusite.com.br">
-        <meta name="twitter:creator" content="@rudrigufelipe">
-        <meta name="twitter:title" content="tirulo do site">
-        <meta name="twitter:description" content="descricao.">
-        <meta name="twitter:image" content="http://www.meusite.com.br/images/thumbnail-share.jpg">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="assets/css/normalize.min.css" />
-        <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="screen" />
-
-        <!-- Scripts -->
-        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-    </head>
+<?php include_once "_inc/headlines.php"; ?>
 
 
     <body class="home">
-        <!--[if lt IE 8]>
+        <!--[if lt IE 10]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
@@ -78,6 +14,17 @@
 
 
             <div class="main">
+                <nav class="mapa-template">
+                    <h2>ELEMENTOS</h2>
+
+                    <ul>
+                        <li><a href="elements/botoes.php">Botões</a></li>
+                    </ul>
+                </nav><!-- mapa-template -->
+
+
+
+
                 <ul class="breadcrumb">
                   <li><a href="#">Home</a> <span class="divider">/</span></li>
                   <li><a href="#">Library</a> <span class="divider">/</span></li>
@@ -186,20 +133,6 @@
 
         </div><!-- end all -->
 
-        <!-- jQuery online -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
-        <script src="assets/js/plugins.js"></script>
-        <script src="assets/js/main.js"></script>
-
-        <!-- Google Analytics -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
+        <?php include_once "_inc/scripts.php"; ?>
     </body>
 </html>
